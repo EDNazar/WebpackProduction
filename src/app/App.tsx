ed8df3aab722fe003d5b2c85e74cfeed0187e00d
EDNazar <neislawdifry@gmail.com>
2026-01-10 15:01:50 +0300

    add babel, add extract plugin

---
+++ src/app/App.tsx
@@ -0,0 +1,29 @@
+
+import './styles/index.scss'
+import { classNames } from 'shared/lib/classNames/classNames'
+import { useTheme } from 'app/providers/ThemeProvider'
+import { Navbar } from 'widgets/Navbar'
+import { Sidebar } from 'widgets/Sidebar'
+import { AppRouter } from 'app/providers/router'
+import { Suspense } from 'react'
+import { useTranslation } from 'react-i18next'
+
+
+
+const App = () => {
+  const {theme} = useTheme()
+
+  return (
+    <div className={classNames('app', {}, [theme])}>
+      <Suspense fallback="">
+        <Navbar />
+        <div className="content-page">
+          <Sidebar />
+          <AppRouter />
+        </div>
+      </Suspense>
+    </div>
+  )
+}
+
+export default App
