e051ec379cf8ea0ddc1b296f38ace0ea81ec7ae8
EDNazar <neislawdifry@gmail.com>
2026-01-11 14:36:55 +0300

    eslint, stylelint install and fix problems

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,29 +1,25 @@
-
-import './styles/index.scss'
-import { classNames } from 'shared/lib/classNames/classNames'
-import { useTheme } from 'app/providers/ThemeProvider'
-import { Navbar } from 'widgets/Navbar'
-import { Sidebar } from 'widgets/Sidebar'
-import { AppRouter } from 'app/providers/router'
-import { Suspense } from 'react'
-import { useTranslation } from 'react-i18next'
-
-
+import './styles/index.scss';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTheme } from 'app/providers/ThemeProvider';
+import { Navbar } from 'widgets/Navbar';
+import { Sidebar } from 'widgets/Sidebar';
+import { AppRouter } from 'app/providers/router';
+import { Suspense } from 'react';
 
 const App = () => {
-  const {theme} = useTheme()
+    const { theme } = useTheme();
 
-  return (
-    <div className={classNames('app', {}, [theme])}>
-      <Suspense fallback="">
-        <Navbar />
-        <div className="content-page">
-          <Sidebar />
-          <AppRouter />
+    return (
+        <div className={classNames('app', {}, [theme])}>
+            <Suspense fallback="">
+                <Navbar />
+                <div className="content-page">
+                    <Sidebar />
+                    <AppRouter />
+                </div>
+            </Suspense>
         </div>
-      </Suspense>
-    </div>
-  )
-}
+    );
+};
 
-export default App
+export default App;
--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -1,6 +1,6 @@
-import { classNames } from 'shared/lib/classNames/classNames'
-import cls from './Button.module.scss'
-import { ButtonHTMLAttributes, FC } from 'react'
+import { classNames } from 'shared/lib/classNames/classNames';
+import { ButtonHTMLAttributes, FC } from 'react';
+import cls from './Button.module.scss';
 
 export enum ThemeButton {
     CLEAR = 'clear',
@@ -13,17 +13,19 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 }
 
 export const Button: FC<ButtonProps> = (props) => {
-  const {
-    className,
-    children,
-    theme,
-    ...otherProps
-  } = props
-  return (
-    <button className={classNames(cls.Button, {[cls[theme]]: true}, [className])}
-    {...otherProps}
-    >
-        {children}
-    </button>
-  )
-}
+    const {
+        className,
+        children,
+        theme,
+        ...otherProps
+    } = props;
+    return (
+        <button
+            type="button"
+            className={classNames(cls.Button, { [cls[theme]]: true }, [className])}
+            {...otherProps}
+        >
+            {children}
+        </button>
+    );
+};
