17316aa5b01fca41ad1e6e0f1aef75e2b21f2b4b
EDNazar <neislawdifry@gmail.com>
2026-02-12 21:03:19 +0300

    add json-server

--- src/entities/Counter/ui/Counter.tsx
+++ src/entities/Counter/ui/Counter.tsx
@@ -1,13 +1,13 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { Button } from 'shared/ui/Button/Button';
+import { useTranslation } from 'react-i18next';
 import { counterActions } from '../model/slice/counterSlice';
 import { getCounterValue } from '../model/selectors/getCounterValue/getCounterValue';
-import { useTranslation } from 'react-i18next';
 
 export const Counter = () => {
     const dispatch = useDispatch();
     const counterValue = useSelector(getCounterValue);
-    const {t} = useTranslation()
+    const { t } = useTranslation();
 
     const increment = () => {
         dispatch(counterActions.increment());
